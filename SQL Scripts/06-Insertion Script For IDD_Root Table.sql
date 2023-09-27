f111c2df4889b7d0a7867825ac0fe61022dd9dde
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-27 12:26:42 +0530

    Added data insertion SQL script for idd_root table.

---
+++ SQL Scripts/06-Insertion Script For IDD_Root Table.sql
@@ -0,0 +1,10 @@
+insert into idd_root(cca2, root) values("ZA","+2");
+insert into idd_root(cca2, root) values("SJ","+4");
+insert into idd_root(cca2, root) values("WS","+6");
+insert into idd_root(cca2, root) values("GM","+2");
+insert into idd_root(cca2, root) values("KN","+1");
+insert into idd_root(cca2, root) values("MC","+3");
+insert into idd_root(cca2, root) values("AW","+2");
+insert into idd_root(cca2, root) values("GW","+2");
+insert into idd_root(cca2, root) values("KP","+8");
+insert into idd_root(cca2, root) values("PY","+5");
