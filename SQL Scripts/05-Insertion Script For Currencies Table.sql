7ce6e0e3e018658034e2fcc8dc54f3165102805b
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-27 12:25:56 +0530

    Added data insertion SQL script for currencies table.

---
+++ SQL Scripts/05-Insertion Script For Currencies Table.sql
@@ -0,0 +1,10 @@
+insert into currencies(cca2,currencycode,name,symbol) values("ZA","ZAR","South African rand","R");
+insert into currencies(cca2,currencycode,name,symbol) values("SJ","NOK","krone","kr");
+insert into currencies(cca2,currencycode,name,symbol) values("WS","WST","Samoan tālā","T");
+insert into currencies(cca2,currencycode,name,symbol) values("GM","GMD","dalasi","D");
+insert into currencies(cca2,currencycode,name,symbol) values("KN","XCD","Eastern Caribbean dollar","$");
+insert into currencies(cca2,currencycode,name,symbol) values("MC","EUR","Euro","€");
+insert into currencies(cca2,currencycode,name,symbol) values("AW","AWG","Aruban florin","ƒ");
+insert into currencies(cca2,currencycode,name,symbol) values("GW","XOF","West African CFA franc","Fr");
+insert into currencies(cca2,currencycode,name,symbol) values("KP","KPW","North Korean won","₩");
+insert into currencies(cca2,currencycode,name,symbol) values("PY","PYG","Paraguayan guaraní","₲");
