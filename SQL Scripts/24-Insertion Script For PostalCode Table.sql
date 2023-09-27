e5fe50094ff3d823bcce0f5dcbea36065d654219
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-27 12:40:55 +0530

    Added data insertion SQL script for postalcode table.

---
+++ SQL Scripts/24-Insertion Script For PostalCode Table.sql
@@ -0,0 +1,5 @@
+insert into postalcode(cca2,format,regex) values("ZA","####","^(\\d{4})$");
+insert into postalcode(cca2,format,regex) values("MC","#####","^(\\d{5})$");
+insert into postalcode(cca2,format,regex) values("GW","####","^(\\d{4})$");
+insert into postalcode(cca2,format,regex) values("KP","###-###","^(\\d{6})$");
+insert into postalcode(cca2,format,regex) values("PY","####","^(\\d{4})$");
