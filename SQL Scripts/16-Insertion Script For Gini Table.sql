d41a77eb17b32c07bd8783ccfbac6afe8065ff1f
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-27 12:34:54 +0530

    Added data insertion SQL script for gini table.

---
+++ SQL Scripts/16-Insertion Script For Gini Table.sql
@@ -0,0 +1,5 @@
+insert into gini(cca2,year,value) values("ZA","2014",63.0);
+insert into gini(cca2,year,value) values("WS","2013",38.7);
+insert into gini(cca2,year,value) values("GM","2015",35.9);
+insert into gini(cca2,year,value) values("GW","2010",50.7);
+insert into gini(cca2,year,value) values("PY","2019",45.7);
