99302d7af87e842e432eb908abc5a656727fb833
Pritam Saha <saha.pritam.work@gmail.com>
2023-09-27 12:28:50 +0530

    Added data insertion SQL script for capital table.

---
+++ SQL Scripts/08-Insertion Script For Capital Table.sql
@@ -0,0 +1,12 @@
+insert into capital(cca2,capitalname) values("ZA","Pretoria");
+insert into capital(cca2,capitalname) values("ZA","Bloemfontein");
+insert into capital(cca2,capitalname) values("ZA","Cape Town");
+insert into capital(cca2,capitalname) values("SJ","Longyearbyen");
+insert into capital(cca2,capitalname) values("WS","Apia");
+insert into capital(cca2,capitalname) values("GM","Banjul");
+insert into capital(cca2,capitalname) values("KN","Basseterre");
+insert into capital(cca2, capitalname) values("MC","Monaco");
+insert into capital(cca2, capitalname) values("AW","Oranjestad");
+insert into capital(cca2, capitalname) values("GW","Bissau");
+insert into capital(cca2, capitalname) values("KP","Pyongyang");
+insert into capital(cca2, capitalname) values("PY","Asunción");
