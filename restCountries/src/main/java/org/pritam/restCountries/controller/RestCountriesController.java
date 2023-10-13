3fa3bb4be7d59b9222aa075af9c25a87a4d9e847
Pritam Saha <saha.pritam.work@gmail.com>
2023-10-13 12:57:21 +0530

    Added documentation for the country endpoints

--- restCountries/src/main/java/org/pritam/restCountries/controller/RestCountriesController.java
+++ restCountries/src/main/java/org/pritam/restCountries/controller/RestCountriesController.java
@@ -35,11 +35,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Api(tags = "3. Country Endpoints", description = "Contain Info On Endpoints For Country Related Details. ")
 @RestController
@@ -124,8 +129,14 @@ public class RestCountriesController {
 		return countryList;
 	}
 
+	@ApiOperation(value = "Get All Countries",notes = "This endpoint returns all the courtries and their related details available in the database.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/all")
-	public ResponseEntity<Object> all() {
+	public ResponseEntity<Object> all(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization) {
 		List<String> cca2List = null;
 		cca2List = countryService.getAllCca2();
 		
@@ -138,9 +149,16 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "Get Countries By Name",notes = "This endpoint returns all the courtries and their related details based on searching by country name. If you want to get an exact match, use the /v3.1/name/{name}?fullText=true endpoint. It can be the common or official value.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/name/{name}")
-	public ResponseEntity<Object> name(@PathVariable("name") String name,
-			@RequestParam(name = "fullText", required = false) boolean fullText) {
+	public ResponseEntity<Object> name(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be the common or official value.") @PathVariable("name") String name,
+			@ApiParam(value = "It is optional. Passing it true means the name passed in path is a full name.") @RequestParam(name = "fullText", required = false) boolean fullText) {
 		List<String> cca2List = null;
 
 		if (fullText)
@@ -157,8 +175,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Country By Code",notes = "This endpoint returns the courtry and its related details based on searching by cca2, ccn3, cca3 or cioc country code (yes, any!).", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/alpha/{code}")
-	public ResponseEntity<Object> code(@PathVariable("code") String code) {
+	public ResponseEntity<Object> code(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be any cca2, ccn3, cca3 or cioc country code.") @PathVariable("code") String code) {
 		List<String> cca2List = null;
 		cca2List = countryService.getCountryByCode(code);
 
@@ -171,8 +196,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By List Of Code",notes = "This endpoint returns all the courtries and their related details based on the code list provided. Code list can compose of cca2, ccn3, cca3 or cioc country code (yes, any!).", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/alpha")
-	public ResponseEntity<Object> listOfCode(@RequestParam(name="codes",required = true)List<String> codes) {
+	public ResponseEntity<Object> listOfCode(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can have list of codes separated by comma. Code can be any cca2, ccn3, cca3 or cioc country code.") @RequestParam(name="codes",required = true)List<String> codes) {
 		List<String> cca2List = null;
 		cca2List = countryService.getCountryByListOfCodes(codes);
 
@@ -185,8 +217,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Currency",notes = "This endpoint returns all the courtries and their related details based on the currency code or name provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/currency/{currency}")
-	public ResponseEntity<Object> currency(@PathVariable("currency") String currencyCodeOrName) {
+	public ResponseEntity<Object> currency(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be any currency code or name.") @PathVariable("currency") String currencyCodeOrName) {
 		List<String> cca2List = null;
 		cca2List = currencyService.getCountryByCurrencyCodeOrName(currencyCodeOrName);
 
@@ -199,8 +238,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Demonym",notes = "This endpoint returns all the courtries and their related details based on the demonym provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/demonym/{demonym}")
-	public ResponseEntity<Object> demonym(@PathVariable("demonym") String demonym) {
+	public ResponseEntity<Object> demonym(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be any demonym.") @PathVariable("demonym") String demonym) {
 		List<String> cca2List = null;
 		cca2List = demonymsService.getCountryByDemonym(demonym);
 
@@ -213,8 +259,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Language Code Or Name",notes = "This endpoint returns all the courtries and their related details based on the language name or code provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/lang/{language}")
-	public ResponseEntity<Object> language(@PathVariable("language") String languageCodeOrName) {
+	public ResponseEntity<Object> language(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be any language code or name.") @PathVariable("language") String languageCodeOrName) {
 		List<String> cca2List = null;
 		cca2List = languagesService.getCountryByLanguagesCodeOrName(languageCodeOrName);
 
@@ -227,8 +280,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Capital City",notes = "This endpoint return the courtry and its related details based on the capital city provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/capital/{capital}")
-	public ResponseEntity<Object> capital(@PathVariable("capital") String capitalName) {
+	public ResponseEntity<Object> capital(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be the name of any capital city.") @PathVariable("capital") String capitalName) {
 		List<String> cca2List = null;
 		cca2List = capitalService.getCountryByCapitalName(capitalName);
 
@@ -241,8 +301,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Region",notes = "This endpoint returns all the courtries and their related details based on the region provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/region/{region}")
-	public ResponseEntity<Object> region(@PathVariable("region") String regionName) {
+	public ResponseEntity<Object> region(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be the name of any region.") @PathVariable("region") String regionName) {
 		List<String> cca2List = null;
 		cca2List = countryService.getCountryByRegionName(regionName);
 
@@ -255,8 +322,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Sub Region",notes = "This endpoint returns all the courtries and their related details based on the sub region provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/subregion/{subregion}")
-	public ResponseEntity<Object> subRegion(@PathVariable("subregion") String subRegionName) {
+	public ResponseEntity<Object> subRegion(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be the name of any sub region.") @PathVariable("subregion") String subRegionName) {
 		List<String> cca2List = null;
 		cca2List = countryService.getCountryBySubRegionName(subRegionName);
 
@@ -269,8 +343,15 @@ public class RestCountriesController {
 		return new ResponseEntity<>(countryList, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Get Countries By Translation Name",notes = "This endpoint returns all the courtries and their related details based on the translation name provided.", produces = "application/json")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "The request has been successfully executed", response = Country.class, responseContainer = "List"),
+		@ApiResponse(code = 404, message = "No countries available in the database.",response = Error.class),
+		@ApiResponse(code = 401, message = "Some issue related to Authorization header or JWT.",response = Error.class)
+	})
 	@GetMapping("/translation/{translation}")
-	public ResponseEntity<Object> translation(@PathVariable("translation") String translationOfficialOrCommon) {
+	public ResponseEntity<Object> translation(@ApiParam(value = "It should be a bearer token.") @RequestHeader("Authorization") String authorization,
+			@ApiParam(value = "It can be any translation name.") @PathVariable("translation") String translationOfficialOrCommon) {
 		List<String> cca2List = null;
 		cca2List = translationsService.getCountryByTranslationOfficialOrCommon(translationOfficialOrCommon);
 
